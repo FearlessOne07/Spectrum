ac93ae1ab803bd8a5000bcc3e72b9e961ecf0f1f
Baraka Joel <bjtalems@gmail.com>
2025-05-14 19:23:21 +0100

    Added tweening to ui pause menu buttons

--- src/Scenes/PauseMenu/Layers/MainPauseLayer.cpp
+++ src/Scenes/PauseMenu/Layers/MainPauseLayer.cpp
@@ -2,6 +2,7 @@
 #include "base/assets/AssetManager.hpp"
 #include "base/game/RenderContextSingleton.hpp"
 #include "base/scenes/Scene.hpp"
+#include "base/ui/UIElement.hpp"
 #include "base/ui/UILayer.hpp"
 #include "base/ui/elements/UIButton.hpp"
 #include "raylib.h"
@@ -22,26 +23,48 @@ void MainPauseLayer::OnAttach()
   resumeButton->SetFont(GetOwner()->GetAssetManager()->GetAsset<Font>("main-font-normal"));
   resumeButton->SetText("Resume");
   resumeButton->SetFontSize(40);
+  resumeButton->SetAnchorPoint(Base::UIElement::AnchorPoint::CENTER);
   resumeButton->SetPosition( //
     {
-      (rd->gameWidth / 2) - (resumeButton->GetSize().x / 2),
-      ((rd->gameHeight / 2) - (resumeButton->GetSize().y / 2)) - 100,
+      rd->gameWidth / 2,
+      (rd->gameHeight / 2) - 100,
     } //
   );
   resumeButton->onClick = [this]() { GetOwner()->SetSceneTransition(Base::SceneRequest::POP_CURRENT_SCENE); };
+  resumeButton->onHover = {
+    [=, this]() { //
+      GetOwner()->GetTweenManager()->AddTween<float>(
+        resumeButton.get(), [=](float size) { resumeButton->SetFontSize(size); }, 40, 45, 0.1);
+    },
+    [=, this]() { //
+      GetOwner()->GetTweenManager()->AddTween<float>(
+        resumeButton.get(), [=](float size) { resumeButton->SetFontSize(size); }, resumeButton->GetFontSize(), 40, 0.1);
+    },
+  };
 
   // Exit Button
   auto exitButton = mainLayer.AddElement<Base::UIButton>("exit-button");
   exitButton->SetFont(GetOwner()->GetAssetManager()->GetAsset<Font>("main-font-normal"));
   exitButton->SetText("Exit");
   exitButton->SetFontSize(40);
+  exitButton->SetAnchorPoint(Base::UIElement::AnchorPoint::CENTER);
   exitButton->SetPosition( //
     {
-      (rd->gameWidth / 2) - (exitButton->GetSize().x / 2),
-      ((rd->gameHeight / 2) - (exitButton->GetSize().y / 2)),
+      rd->gameWidth / 2,
+      rd->gameHeight / 2,
     } //
   );
   exitButton->onClick = [this]() { GetOwner()->SetSceneTransition(Base::SceneRequest::QUIT); };
+  exitButton->onHover = {
+    [=, this]() { //
+      GetOwner()->GetTweenManager()->AddTween<float>(
+        exitButton.get(), [=](float size) { exitButton->SetFontSize(size); }, 40, 45, 0.1);
+    },
+    [=, this]() { //
+      GetOwner()->GetTweenManager()->AddTween<float>(
+        exitButton.get(), [=](float size) { exitButton->SetFontSize(size); }, exitButton->GetFontSize(), 40, 0.1);
+    },
+  };
 }
 
 void MainPauseLayer::OnDetach()
