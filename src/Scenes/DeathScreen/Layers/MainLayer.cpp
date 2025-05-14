c69156c8984f87ad91fa35ae5df7d05877c0a3a9
Baraka Joel <bjtalems@gmail.com>
2025-05-14 12:57:32 +0100

    Added UITweeing

--- src/Scenes/DeathScreen/Layers/MainLayer.cpp
+++ src/Scenes/DeathScreen/Layers/MainLayer.cpp
@@ -3,6 +3,8 @@
 #include "base/game/RenderContext.hpp"
 #include "base/game/RenderContextSingleton.hpp"
 #include "base/scenes/Scene.hpp"
+#include "base/tween/TweenManager.hpp"
+#include "base/ui/UIElement.hpp"
 #include "base/ui/elements/UILabel.hpp"
 
 void MainLayer::OnAttach()
@@ -13,11 +15,19 @@ void MainLayer::OnAttach()
   deathLabel->SetText("YOU DIED!");
   deathLabel->SetFont(GetOwner()->GetAssetManager()->GetAsset<Font>("main-font-normal"));
   deathLabel->SetTextColor(RED);
-  deathLabel->SetFontSize(70);
+  deathLabel->SetFontSize(80);
+  deathLabel->SetAnchorPoint(Base::UIElement::AnchorPoint::CENTER);
   deathLabel->SetPosition({
-    (rd->gameWidth / 2) - (deathLabel->GetSize().x / 2),
-    (rd->gameHeight / 2) - (deathLabel->GetSize().y / 2),
+    rd->gameWidth / 2,
+    rd->gameHeight / 2,
   });
+
+  GetOwner()->GetTweenManager()->AddTween<unsigned char>(
+    deathLabel.get() + 1, [=](unsigned char alpha) { deathLabel->SetTextColor({255, 0, 0, alpha}); }, 0, 255, 1);
+  GetOwner()->GetTweenManager()->AddTween<float>(
+    deathLabel.get() + 2, [=](float pos) { deathLabel->SetPosition({rd->gameWidth / 2, pos}); },
+    (rd->gameHeight / 2) - 200, rd->gameHeight / 2, 1 //
+  );
 }
 
 void MainLayer::Render()
