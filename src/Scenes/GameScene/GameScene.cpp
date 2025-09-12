8c2cc9b3046de164241d55aeee791992f90c95a5
Baraka Joel <bjtalems@gmail.com>
2025-09-12 14:04:19 +0100

    Updated Base

--- src/Scenes/GameScene/GameScene.cpp
+++ src/Scenes/GameScene/GameScene.cpp
@@ -52,7 +52,8 @@ void GameScene::Enter(Base::SceneData sceneData)
   mainLayer->SetCameraZoom(mainLayerRes.x / rd->gameWidth);
   mainLayer->SetCameraTarget({0, 0});
   mainLayer->SetCameraRotation(0);
-
+  
+  // TODO: Fix Tone mapping??
   mainLayer->AddShaderEffect<Bloom>(1.2, 0.25, 1);
 
   AttachLayer<MainGameLayer>(mainLayer);
